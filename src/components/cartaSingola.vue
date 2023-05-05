b0e22d52fbb92d790d2277adacd8cfdc68199efb
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-05-05 17:20:49 +0200

    corretta ricerca e struttura base

--- src/components/cartaSingola.vue
+++ src/components/cartaSingola.vue
@@ -3,6 +3,7 @@
     import {store} from '../store';
     export default {
         name: "cartaSingola",
+        props: ['carteciclate'],
         data(){
             return{
                 store
@@ -15,8 +16,9 @@
 
 <template>
     <div class="carta">
+        <img :src="carteciclate.card_images[0].image_url" alt="">
         <span>
-            {{store.pagina}}
+            {{ carteciclate.desc }}
         </span>
         
     </div>
@@ -25,7 +27,36 @@
 <style lang="scss" scooped>
 
     .carta{
-        width: 100px;
+        flex-basis: calc(100%/ 6 - 5px);
+        display: flex;
+        align-items:center;
+        align-items: center;
+        padding: 2px;
+        position: relative;
+        &:hover span {
+            display: block;
+            cursor: default;
+        }
+
+        img {
+            width: 100%;
+            border: 1px solid rgb(35, 31, 31);
+            box-shadow: 3px 17px 18px -1px #000000;
+            display: block;
+        }
+        span{
+            position: absolute;
+            text-transform: uppercase;
+            top:60px;
+            left: 2px;
+            right:2px;
+            background-color: rgba(128, 128, 128, 0.845);
+            color: white;
+            line-height: 0.8rem;
+            font-size: small;
+            padding: 10px;
+            display: none;
+        }
 
     }
 </style>
--- src/components/mainComponent.vue
+++ src/components/mainComponent.vue
@@ -18,25 +18,18 @@
     },
     methods: {
         chiamataApi() {
-            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=12&offset=0')
             .then( (res) =>{
                 this.store.arrayCarte = res.data.data
-
-                this.paginaVisualizzata()
             })
-        },
-        paginaVisualizzata() {
-            this.store.pagina = this.store.arrayCarte.filter((element, index) => index >= 0 && index < 15 )
-            console.log(this.store.pagina)
-         }
-
+        },       
     }
     }
 </script>
 
 <template>
     <div class="boxCard">
-        <cartaSingola v-for="(element, index) in store.pagina"/>
+        <cartaSingola v-for="(element, index) in store.arrayCarte" :carteciclate="element" />
         
     </div>
 </template>
@@ -44,9 +37,12 @@
 <style lang="scss" scooped>
     .boxCard{
         width: 80%;
-        margin: auto;
-        border: 1px solid black;
+        margin: 0 auto;
         height: 100%;
-        padding: 30px;
+        padding: 10px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        flex-wrap: wrap;
     }
 </style>
