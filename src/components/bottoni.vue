3e35cb4347123bac61104ee10f1a8f4355c9088b
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-05-07 22:25:41 +0200

    COMPLETO con filtri e cambio pagina

--- src/components/bottoni.vue
+++ src/components/bottoni.vue
@@ -43,12 +43,13 @@
                     console.log(store.numero)
                     })     
                 } else {
+
                     store.numero -= 12           
-                        store.pathOnScreen = store.pathBase + store.pathFilter + store.filterArchetype + store.pathPagina + store.numero
-                        axios.get(store.pathOnScreen)
-                        .then( (res) =>{
-                        this.store.arrayCarte = res.data.data
-                        console.log(store.numero)
+                    store.pathOnScreen = store.pathBase + store.pathFilter + store.filterArchetype + store.pathPagina + store.numero
+                    axios.get(store.pathOnScreen)
+                    .then( (res) =>{
+                    this.store.arrayCarte = res.data.data
+                    console.log(store.numero)
                         })
                     }                   
                 }
@@ -58,7 +59,7 @@
 </script>
 
 <template>
-<button class="right" @click="avanti">avanti</button>
+<button v-if="(store.filteredArrayLength - store.numero) > 11"  class="right" @click="avanti">avanti</button>
 <button v-if="store.numero > 0" class="left"  @click="indietro">indietro</button>
 </template>
 
